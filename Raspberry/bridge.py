00ccbef0932246d245ca21df39f879fee2aa05f9
Nerlih Max <nerlihmax@yandex.ru>
2021-04-15 18:16:35 +0700

    гитигнор

--- Raspberry/bridge.py
+++ Raspberry/bridge.py
@@ -1,12 +1,12 @@
 # DEVICE_ID table:
-# 0x01 | servo-motor
+# 0x01 | servo-motor 
 # 0x02 | drive motor
 # 0x0A | left range sensor
 # 0x0B | right range sensor
 
 # scheme of data for sending to controller:
 # 1 byte - DEVICE_ID
-# 4 byte - DATA
+# 2 byte - DATA (if requied)
 
 # scheme of data for getting from controller:
-# 4 byte - DATA
+# 2 byte - DATA (if requied)
