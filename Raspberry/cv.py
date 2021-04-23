ff320168dd9800536b1f5daca52036b0a7b7de6a
Nerlih Max <nerlihmax@yandex.ru>
2021-04-23 19:26:16 +0700

    потихоньку дебажу, оформил в модуль(вроде)

--- Raspberry/cv.py
+++ Raspberry/cv.py
@@ -3,23 +3,27 @@ import numpy as np
 
 # im = cv2.imread('res/example.png')
 
-# def nop(x):
-#     pass
+def nop(x):
+    pass
 
 # def mouse_event(event, x, y, flags, params):
 #     if event == cv2.EVENT_LBUTTONDBLCLK:
 #         print(f'({x}, {y}) = {hsv[y][x]}')
 
-# cv2.namedWindow('controls')
-# cv2.createTrackbar('red_h_min', 'controls', 0, 255, nop)
-# cv2.createTrackbar('red_h_max', 'controls', 0, 255, nop)
-# cv2.createTrackbar('red_s_min', 'controls', 0, 255, nop)
-# cv2.createTrackbar('red_s_max', 'controls', 0, 255, nop)
-# cv2.createTrackbar('red_v_min', 'controls', 0, 255, nop)
-# cv2.createTrackbar('red_v_max', 'controls', 0, 255, nop)
+cv2.namedWindow('controls')
+cv2.createTrackbar('red_h_min', 'controls', 0, 255, nop)
+cv2.createTrackbar('red_h_max', 'controls', 0, 255, nop)
+cv2.createTrackbar('red_s_min', 'controls', 0, 255, nop)
+cv2.createTrackbar('red_s_max', 'controls', 0, 255, nop)
+cv2.createTrackbar('red_v_min', 'controls', 0, 255, nop)
+cv2.createTrackbar('red_v_max', 'controls', 0, 255, nop)
+
 # cv2.setMouseCallback('im', mouse_event)
 
-cap = cv2.VideoCapture(0)
+COLOR_RED = 0
+COLOR_GREEN = 1
+
+cap = cv2.VideoCapture('http://192.168.2.153:8000/camera/mjpeg')
 
 def find_objects():
     _, im = cap.read()
@@ -27,50 +31,73 @@ def find_objects():
     blurred = cv2.GaussianBlur(im, (9, 9), 0)
     hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
-    # red_mask_min = (cv2.getTrackbarPos('red_h_min', 'controls'), cv2.getTrackbarPos('red_s_min', 'controls'), cv2.getTrackbarPos('red_v_min', 'controls'))
-    # red_mask_max = (cv2.getTrackbarPos('red_h_max', 'controls'), cv2.getTrackbarPos('red_s_max', 'controls'), cv2.getTrackbarPos('red_v_max', 'controls'))
-    red_mask_min = (179-10, 0, 0)
-    red_mask_max = (179+10, 255, 255)
+    green_mask_min = (cv2.getTrackbarPos('red_h_min', 'controls'), cv2.getTrackbarPos('red_s_min', 'controls'), cv2.getTrackbarPos('red_v_min', 'controls'))
+    green_mask_max = (cv2.getTrackbarPos('red_h_max', 'controls'), cv2.getTrackbarPos('red_s_max', 'controls'), cv2.getTrackbarPos('red_v_max', 'controls'))
+    red_mask_min = (0, 0, 0)
+    red_mask_max = (25, 255, 255)
 
     # green_mask_min = (69-10, 0, 0)
     # green_mask_max = (69+10, 255, 255)
 
     thresholded_red = cv2.inRange(hsv, red_mask_min, red_mask_max)
-    # thresholded_green = cv2.inRange(hsv, green_mask_min, green_mask_max)
+    thresholded_green = cv2.inRange(hsv, green_mask_min, green_mask_max)
 
     resulting = im.copy()
 
-    # contours_red, hierarchy_red = cv2.findContours(thresholded_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_red, hierarchy_red = cv2.findContours(thresholded_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_green, hierarchy_green = cv2.findContours(thresholded_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # cv2.drawContours(resulting, contours_red, -1, (0,255,0), 3)
-    # print(contours_red)
 
-    # for cnt in contours_red:
-    #     rect = cv2.minAreaRect(cnt)
-    #     box = cv2.boxPoints(rect)
-    #     box = np.int0(box)
-    #     area = int(rect[1][0]*rect[1][1])
-    #     if area > 100:
-    #         cv2.drawContours(resulting,[box],0,(255,0,0),2)
-    #         x1 = (box[1][0] + box[0][0]) / 2
-    #         x2 = (box[2][0] + box[3][0]) / 2
-    #         # y1 = (box[1][1] + box[2][1]) / 2
-    #         # y2 = (box[0][1] + box[3][1]) / 2
-    #         x = int((x1 + x2) / 2)
-    #         # y = int((y1 + y2) / 2)
-    #         cv2.line(resulting, (x, 0), (x, 500), (255, 0, 0), 2)
+    objects = []
+
+    for cnt in contours_red:
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        area = int(rect[1][0]*rect[1][1])
+        if area > 1500:
+            print(area)
+            cv2.drawContours(resulting,[box],0,(255,0,0),2)
+            x1 = (box[1][0] + box[0][0]) / 2
+            x2 = (box[2][0] + box[3][0]) / 2
+            # y1 = (box[1][1] + box[2][1]) / 2
+            # y2 = (box[0][1] + box[3][1]) / 2
+            x = int((x1 + x2) / 2)
+            # y = int((y1 + y2) / 2)
+            cv2.line(resulting, (x, 0), (x, 500), (255, 0, 0), 2)
+            objects.append((COLOR_RED, x))
+
+    for cnt in contours_green:
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        area = int(rect[1][0]*rect[1][1])
+        if area > 1500:
+            print(area)
+            cv2.drawContours(resulting,[box],0,(255,0,0),2)
+            x1 = (box[1][0] + box[0][0]) / 2
+            x2 = (box[2][0] + box[3][0]) / 2
+            # y1 = (box[1][1] + box[2][1]) / 2
+            # y2 = (box[0][1] + box[3][1]) / 2
+            x = int((x1 + x2) / 2)
+            # y = int((y1 + y2) / 2)
+            cv2.line(resulting, (x, 0), (x, 500), (255, 0, 0), 2)
+            objects.append((COLOR_GREEN , x))
 
     # cv2.imshow('im', im)
     # cv2.imshow('blurred', blurred)
-    cv2.imshow('red_mask', thresholded_red)
-    # cv2.imshow('green_mask', thresholded_green)
-    # cv2.imshow('resulting', resulting)
+    # cv2.imshow('red_mask', thresholded_red)
+    cv2.imshow('green_mask', thresholded_green)
+    cv2.imshow('resulting', resulting)
 
+    return objects
 
 if __name__ == '__main__':
     while True:
-        find_objects()
+        print(find_objects())
 
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
-        cv2.destroyAllWindows()
+
+    cv2.destroyAllWindows()
